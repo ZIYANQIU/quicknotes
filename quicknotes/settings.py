aa9dd74b4c713f388c9b19743ffcb66bc1f07d44
Elvira Qiu <ziyanqiu.elvira@gmail.com>
2025-12-07 18:41:03 -0800

    modify host

--- quicknotes/settings.py
+++ quicknotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kj97db9b_hzu=hi@04b#z=_($l36v()x+bq&%+e54v8@*ycsh9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quicknotes-3i52.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['quicknotes-irkk.onrender.com', '127.0.0.1']
 
 
 # Application definition
